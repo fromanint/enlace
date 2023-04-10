5e94c8f507179eb4c3d7d951ec0a4b06ffbe4d5c
Froman <Fromaninteractive@gmail.com>
2023-04-10 12:29:49 -0500

    finished

--- Liverpool new/Class/Etiquetas_embarque.cs
+++ Liverpool new/Class/Etiquetas_embarque.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using System.Data;
 using System.IO;
 using Xceed.Words.NET;
+using System.Diagnostics;
+
 
 namespace Liverpool_new.Class
 {
@@ -14,13 +16,24 @@ namespace Liverpool_new.Class
         DataSet ListaTiendas = new DataSet();
         ControlTiendas DetalleTiendas = new ControlTiendas();
         List<int> tiendaspedido = new List<int>();
+        List<string> contenedores = new List<string>();
 
         DocX oWordDoc;
 
-        public string Crear_Etiquetas_Embarque(List<int> tiendas, string pedido, string fecha, int eti_x_hoja = 8)
+        public string Crear_Etiquetas_Embarque(Dictionary<int, List<string>> tiendaContenedores, string pedido, string fecha, int eti_x_hoja = 8)
         {
             bool non = false;
-            tiendaspedido = tiendas;
+            tiendaspedido.Clear(); // Limpiar lista tiendaspedido
+            foreach (var tc in tiendaContenedores)
+            {
+                int tienda = tc.Key;
+                List<string> conte = tc.Value;
+                foreach (string contenedor in conte)
+                {
+                    tiendaspedido.Add(tienda); // Agregar número de tienda
+                    contenedores.Add(contenedor); // Agregar contenedor
+                }
+            }
             int repeticiones = (tiendaspedido.Count - 1) / eti_x_hoja;
             string msg = "";
             if (tiendaspedido.Count % 2 != 0)
@@ -46,14 +59,15 @@ namespace Liverpool_new.Class
             {
                 List<string> lines = File.ReadAllLines(rutaArchivoCSV).ToList();
                 Dictionary<int, List<string>> tiendaContenedores = new Dictionary<int, List<string>>();
-
+                string ordenCompra = "";
+                string fechaEntrega = "";
                 for (int i = 2; i < lines.Count; i++)
                 {
                     string[] columns = lines[i].Split(',');
 
-                    int ordenCompra = int.Parse(columns[0]);
+                    ordenCompra = columns[0];
                     int tienda = int.Parse(columns[2]);
-                    string fechaEntrega = columns[11];
+                    fechaEntrega = columns[11];
                     string contenedor = columns[15];
 
                     if (!tiendaContenedores.ContainsKey(tienda))
@@ -66,15 +80,18 @@ namespace Liverpool_new.Class
                         tiendaContenedores[tienda].Add(contenedor);
                     }
                 }
-
-                tiendaspedido.Clear();
-                foreach (var item in tiendaContenedores)
-                {
-                    tiendaspedido.Add(item.Key);
-
+                if(ordenCompra != "" && fechaEntrega!= "")
+                { 
+                return Crear_Etiquetas_Embarque(tiendaContenedores, ordenCompra, fechaEntrega, 8);
+                }
+                else
+                { if (ordenCompra == "")
+                        return ("Hay un problema con la orden de compra");
+                    else
+                        return ("Hay un problema con la fecha de entrega");
                 }
 
-                return "";
+
             }
             catch (Exception ex)
             {
@@ -95,26 +112,26 @@ namespace Liverpool_new.Class
                 {
                     index = i;
                     InsertarPlantilla(plantilla1);
-                    ReemplazarPlantilla(tiendaspedido[i]);
+                    ReemplazarPlantilla(tiendaspedido[i],i+1);
                     for (int j = 1; j < max_eti; j++)
                     {
                         index += max;
-                        ReemplazarPlantilla(tiendaspedido[index], "XX2", "YY2", "TIENDA2");
+                        ReemplazarPlantilla(tiendaspedido[index],index+1, "XX2", "YY2", "TIENDA2", "CONT-Y");
                         index += max;
                         InsertarPlantilla(plantilla1);
-                        ReemplazarPlantilla(tiendaspedido[index]);
+                        ReemplazarPlantilla(tiendaspedido[index], index + 1);
                     }
                     index += max;
-                    ReemplazarPlantilla(tiendaspedido[index], "XX2", "YY2", "TIENDA2");
+                    ReemplazarPlantilla(tiendaspedido[index], index + 1, "XX2", "YY2", "TIENDA2", "CONT-Y");
                 }
                 index++;
                 for (int i = index; i < tiendaspedido.Count - 1; i++)
                 {
                     index = i;
                     InsertarPlantilla(plantilla1);
-                    ReemplazarPlantilla(tiendaspedido[i]);
+                    ReemplazarPlantilla(tiendaspedido[i],i+1);
                     i++;
-                    ReemplazarPlantilla(tiendaspedido[i], "XX2", "YY2", "TIENDA2");
+                    ReemplazarPlantilla(tiendaspedido[i],i+1, "XX2", "YY2", "TIENDA2", "CONT-Y");
                 }
                 if (non)
                 {
@@ -125,23 +142,41 @@ namespace Liverpool_new.Class
                 BuscarReemplazar("12345678", pedido);
                 return "";
             }
-            catch { return "Ha ocurrido un error al generar el archivo"; }
+            catch (Exception ex) // Añade la variable ex para capturar la excepción
+            {
+                return "Ha ocurrido un error al generar el archivo: " + ex.Message + "\n" + ex.StackTrace;
+            }
 
         }
         void InsertarPlantilla(string path)
         {
             using (DocX plantilla = DocX.Load(path))
             {
-                oWordDoc.InsertDocument(plantilla, false);
+                // Encuentra el índice del último párrafo del documento actual
+                int lastIndex = oWordDoc.Paragraphs.Count - 1;
+
+                // Si el documento actual no está vacío, inserta un salto de página antes de la plantilla
+                if (lastIndex >= 0)
+                {
+                    // Inserta un salto de página en el último párrafo del documento
+                  //  oWordDoc.Paragraphs[lastIndex].Append("\n");
+                }
+
+                // Inserta el contenido de la plantilla al final del documento
+                oWordDoc.InsertDocument(plantilla, true, false);
             }
         }
 
-        void ReemplazarPlantilla(int no_tienda, string rno_caja = "XX1", string rcant_caja = "YY1", string rtienda = "TIENDA1", int no_caja = 1, int cant_cajas = 1)
+
+        void ReemplazarPlantilla(int no_tienda, int no_caja = 1, string rno_caja = "XX1", string rcant_caja = "YY1", string rtienda = "TIENDA1", string idContenedor = "CONT-X", int cant_cajas = 1)
         {
-            BuscarReemplazar(rno_caja, no_caja.ToString("D2"));
+            // BuscarReemplazar(rno_caja, no_caja.ToString("D2"));
+            BuscarReemplazar(rno_caja, "01");
             BuscarReemplazar(rcant_caja, cant_cajas.ToString("D2"));
             BuscarReemplazar(rtienda, no_tienda.ToString("D3") + "       " + DetalleTiendas.ObtenerNombre(no_tienda.ToString()));
+            BuscarReemplazar(idContenedor, contenedores[no_caja - 1]); // Añadido para reemplazar el contenedor
         }
+
 
         string AbrirDocumento()
         {
@@ -149,13 +184,23 @@ namespace Liverpool_new.Class
             {
                 oWordDoc = DocX.Create("Documento.docx");
                 DetalleTiendas.Iniciar(ListaTiendas);
+
+                // Establecer márgenes personalizados
+                oWordDoc.MarginLeft = 10; // margen izquierdo de 30 pt
+                oWordDoc.MarginRight = 10; // margen derecho de 30 pt
+                oWordDoc.MarginTop = 10; // margen superior de 30 pt
+                oWordDoc.MarginBottom = 10; // margen inferior de 30 pt
+                                            // Cambiar el tamaño del papel a Carta
+                oWordDoc.PageWidth = 612; // Ancho del tamaño Carta en puntos (8.5 pulgadas)
+                oWordDoc.PageHeight = 792; // Altura del tamaño Carta en puntos (11 pulgadas)
                 return "";
             }
-            catch
+            catch (Exception ex)
             {
-                return "No se pudo Abrir el archivo";
+                return "No se pudo Abrir el archivo: " + ex.Message;
             }
         }
+
 
         string GuardarDocumento(string pedido)
         {
@@ -165,8 +210,15 @@ namespace Liverpool_new.Class
                 string path = "Output\\" + pedido;
                 mc.CrearDirectorio(path);
                 path = "\\" + path + "\\" + pedido;
-                oWordDoc.SaveAs(System.Windows.Forms.Application.StartupPath + path + ".docx");
-                return "Archivo Guardado en: " + path + ".docx";
+                string fullPath = Path.GetFullPath(System.Windows.Forms.Application.StartupPath + path + ".docx");
+                oWordDoc.SaveAs(fullPath);
+                // Iniciar Microsoft Word y abrir el documento guardado
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = "winword.exe";
+                startInfo.Arguments = "\"" + fullPath + "\"";
+                Process.Start(startInfo);
+
+                return "Archivo Guardado en: " + fullPath;
             }
             catch { return ("No se ha podido guardar el archivo, presione enter y elija otro nombre"); }
         }
